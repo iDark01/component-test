215b4904687b1d74ec2998683e436b77be92af5b
Alexis <alex200196@hotmail.it>
2021-04-30 18:49:11 -0500

    Initial project template

---
+++ cp-test/src/main/java/com/lxs/cp/test/CpTestApplication.java
@@ -0,0 +1,13 @@
+package com.lxs.cp.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpTestApplication.class, args);
+	}
+
+}
---
+++ cp-test/src/test/java/com/lxs/cp/test/CpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxs.cp.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
