23504f8b4d3491c34ce776eb389fee87c1b38576
Alexis <alex200196@hotmail.it>
2021-05-01 19:40:19 -0500

    Cors added

--- cp-test/src/main/java/com/lxs/cp/test/controller/SubGenreController.java
+++ cp-test/src/main/java/com/lxs/cp/test/controller/SubGenreController.java
@@ -25,7 +25,7 @@ import io.swagger.annotations.ApiOperation;
 @Api(tags = "music-subgenre", value = "SubGenreController")
 @RestController
 @RequestMapping("/cp-test/api/music-subgenre")
-@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
+//@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
 public class SubGenreController {
 	
 	@Autowired
