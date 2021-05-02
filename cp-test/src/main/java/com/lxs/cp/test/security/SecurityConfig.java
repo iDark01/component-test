92c876b9bab64622c96b025796fd70fb7448839f
Alexis <alex200196@hotmail.it>
2021-05-01 22:10:48 -0500

    Testing cors

--- cp-test/src/main/java/com/lxs/cp/test/security/SecurityConfig.java
+++ cp-test/src/main/java/com/lxs/cp/test/security/SecurityConfig.java
@@ -1,46 +1,15 @@
 package com.lxs.cp.test.security;
 
-import java.util.Arrays;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+@EnableWebMvc
+public class SecurityConfig implements WebMvcConfigurer {
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		try {
-			http.cors()
-			.and().csrf()
-			.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and().authorizeRequests().anyRequest().anonymous();
-		} catch (Exception e) {
-			logger.error("SecurityConfig.configure :: {}", e.getMessage());
-		}
-	}
-
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
 	}
 }
