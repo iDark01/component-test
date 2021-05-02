b58f453eba66e3aae70af0ab70f46f2fecba3388
Alexis <alex200196@hotmail.it>
2021-05-01 19:03:45 -0500

    Testing

--- cp-test/src/main/java/com/lxs/cp/test/CpTestApplication.java
+++ cp-test/src/main/java/com/lxs/cp/test/CpTestApplication.java
@@ -2,8 +2,12 @@ package com.lxs.cp.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = "com.lxs.cp.test")
 public class CpTestApplication {
 
 	public static void main(String[] args) {
